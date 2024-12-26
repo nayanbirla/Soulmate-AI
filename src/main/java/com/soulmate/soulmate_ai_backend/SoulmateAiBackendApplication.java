2746de26439ccdfada14f06b1704b8f5f033ef5e
nayanbirla <nayanbirla9893@gmail.com>
2024-12-26 19:34:59 +0530

    basic project structure ready

---
+++ src/main/java/com/soulmate/soulmate_ai_backend/SoulmateAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soulmate.soulmate_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulmateAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulmateAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulmate/soulmate_ai_backend/SoulmateAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulmate.soulmate_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulmateAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d9559d2d5310f4df2962de310efa9a3eedaa12de
nayanbirla <nayanbirla9893@gmail.com>
2024-12-30 16:01:32 +0530

    Add profile to database and fetch it

--- src/main/java/com/soulmate/soulmate_ai_backend/SoulmateAiBackendApplication.java
+++ src/main/java/com/soulmate/soulmate_ai_backend/SoulmateAiBackendApplication.java
@@ -1,13 +1,37 @@
 package com.soulmate.soulmate_ai_backend;
 
+import com.soulmate.soulmate_ai_backend.profiles.Gender;
+import com.soulmate.soulmate_ai_backend.profiles.Profile;
+import com.soulmate.soulmate_ai_backend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoulmateAiBackendApplication {
+public class SoulmateAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoulmateAiBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Profile profile=new Profile(
+				"1",
+				"Nayan",
+				"Birla",
+				22,
+				"Indian",
+				Gender.MALE,
+				"Software Engineer",
+				"abc.jpg",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
+	}
 }
---
+++ src/main/java/com/soulmate/soulmate_ai_backend/profiles/ProfileRepository.java
@@ -0,0 +1,7 @@
+package com.soulmate.soulmate_ai_backend.profiles;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProfileRepository extends MongoRepository<Profile,String> {
+
+}
